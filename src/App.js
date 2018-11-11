f822522f1ab8f50e8d62925115a9c923db807c55
Paul Lee <ppauleee@gmail.com>
2018-11-11 13:05:09 -0800

    Set up firebase account and configuraton settings

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
+import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
-    render() {
-        return (
-            <View>
-                <Text>An app</Text>
-            </View>
-        );
-    }
+	// Lifecycle methods are automatically invoked inside the component
+	componentWillMount() {
+		firebase.initializeApp({
+			apiKey: 'AIzaSyBkmXjYbUdfjPbDpFUB_v6IFJpoaDQ1xvs',
+			authDomain: 'auth-f01a6.firebaseapp.com',
+			databaseURL: 'https://auth-f01a6.firebaseio.com',
+			projectId: 'auth-f01a6',
+			storageBucket: 'auth-f01a6.appspot.com',
+			messagingSenderId: '34880901777'
+		});
+	}
+
+	render() {
+		return (
+			<View>
+				<Header headerText="Authentication" />
+				<LoginForm />
+			</View>
+		);
+	}
 }
 
 export default App;
