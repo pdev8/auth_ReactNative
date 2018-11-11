1acc284d0d54ab8208112a593a8cb722b7ad0a94
Paul Lee <ppauleee@gmail.com>
2018-11-11 14:50:49 -0800

    Added activity spinner for logging in

--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -1,23 +1,51 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
 import firebase from 'firebase';
-import { Button, Card, CardSection, Input } from './common';
+import { Button, Card, CardSection, Input, Spinner } from './common';
 
 class LoginForm extends Component {
-	state = { email: '', password: '', error: '' };
+	state = { email: '', password: '', error: '', loading: false };
 
 	onButtonPress() {
 		const { email, password } = this.state;
 
-		this.setState({ error: '' });
+		this.setState({ error: '', loading: true });
 
 		// Returns a promise - a promise in JS is a construct for handling async code
 		// and will return w/ either a success or failure upon eventual completion
-		firebase.auth().signInWithEmailAndPassword(email, password).catch(() => {
-			firebase.auth().createUserWithEmailAndPassword(email, password).catch(() => {
-				this.setState({ error: 'Authentication Failed.' });
-			});
+		firebase.auth().signInWithEmailAndPassword(email, password)
+		.then(this.onLoginSuccess.bind(this))
+		.catch(() => {
+			firebase
+				.auth()
+				.createUserWithEmailAndPassword(email, password)
+				.then(this.onLoginSuccess.bind(this))
+				.catch(this.onLoginFail.bind(this));
 		});
+	}
+
+	onLoginSuccess() {
+		this.setState({
+			email: '',
+			password: '',
+			error: '',
+			loading: false
+		});
+	}
+
+	onLoginFail() {
+		this.setState({
+			error: 'Authentication Failed.',
+			loading: false
+		});
+	}
+
+	renderButton() {
+		if (this.state.loading) {
+			return <Spinner size={'small'} />;
+		}
+
+		return <Button onPress={this.onButtonPress.bind(this)}>Log in</Button>;
 	}
 
 	render() {
@@ -44,9 +72,7 @@ class LoginForm extends Component {
 
 				<Text style={styles.errorTextStyle}>{this.state.error}</Text>
 
-				<CardSection>
-					<Button onPress={this.onButtonPress.bind(this)}>Log in</Button>
-				</CardSection>
+				<CardSection>{this.renderButton()}</CardSection>
 			</Card>
 		);
 	}
