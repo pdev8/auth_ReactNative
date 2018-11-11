e71ff23ed2a58dc28bbde8f7be60ce377b1d4e91
Paul Lee <ppauleee@gmail.com>
2018-11-11 15:27:27 -0800

    Handles authentication events for checking user's login status

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+	state = { loggedIn: null };
+
 	// Lifecycle methods are automatically invoked inside the component
 	componentWillMount() {
 		firebase.initializeApp({
@@ -15,13 +17,34 @@ class App extends Component {
 			storageBucket: 'auth-f01a6.appspot.com',
 			messagingSenderId: '34880901777'
 		});
+
+		firebase.auth().onAuthStateChanged((user) => {
+			if (user) {
+				this.setState({ loggedIn: true });
+			} else {
+				this.setState({ loggedIn: false });
+			}
+		});
+	}
+
+	renderContent() {
+		switch (this.state.loggedIn) {
+			case true:
+				return (
+				<Button onPress={() => firebase.auth().signOut()}>Log Out</Button>
+				);
+			case false:
+				return <LoginForm />;
+			default:
+				return <Spinner size={'large'} />;
+		}
 	}
 
 	render() {
 		return (
 			<View>
 				<Header headerText="Authentication" />
-				<LoginForm />
+				{this.renderContent()}
 			</View>
 		);
 	}
